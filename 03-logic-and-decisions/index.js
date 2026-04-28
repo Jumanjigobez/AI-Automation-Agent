472edd2d0cdc85ce7d3a90db64b009d30aba64e3
Jumachaje <73429193+Jumanjigobez@users.noreply.github.com>
2026-04-28 13:59:40 +0000

    problem statement first

--- 03-logic-and-decisions/index.js
+++ 03-logic-and-decisions/index.js
@@ -4,5 +4,13 @@ const {chromium} = require("playwright");
     const browser = await chromium.launch({headless: true}),
         page = await browser.newPage();
 
+    await page.goto("https://books.toscrape.com");
+
+    //We want to check for a book "When we collided" and get it's price, else click button next until book found
+
+    const exists = await page.locator("h3").count()
+
+    console.log(exists)
+    
     await browser.close();
 })();
