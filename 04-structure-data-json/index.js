8e2dd28c67cd9cf64451a01e669aefb372d090c8
Jumachaje <73429193+Jumanjigobez@users.noreply.github.com>
2026-05-12 13:40:54 +0000

    Wonderful, challenge done

--- 04-structure-data-json/index.js
+++ 04-structure-data-json/index.js
@@ -10,6 +10,25 @@ const {chromium} = require("playwright");
 
     //We want to return a JSON structured data of all books at first page, in fields of Img, Title, Price
 
-   
+    const booksData = [];
+
+    const books = await page.locator(".product_pod"),//array
+          count = await books.count();
+
+    // console.log(count)
+
+    for(var i=0;i<count;i++){
+        let book = books.nth(i),
+            booksFound = {
+                Img : `https://books.toscrape.com/${await book.locator("img").getAttribute("src")}`,
+                Title : await book.locator("h3 a").innerText(),
+                Price : await book.locator(".price_color").innerText(),
+            }
+
+        // console.log(booksFound.Img, booksFound.Title, booksFound.Price)
+        booksData.push(booksFound);
+    }
+
+    console.log(booksData);
     await browser.close();
 })();
